3dae7c4ba883e6b1ae8173314518c1e6efbddd03
Serge Kushner <kusnir.sergiu@gmail.com>
2018-06-12 17:55:34 +0200

    first cecond ommit

---
+++ EssentialTraining/Simple Array.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+   public class Simple_Array
+    {
+        public string[] GroceryList;
+
+        public Simple_Array()
+        {
+            GroceryList = new string[4] { "Bread", "Milk", "Eggs", "Cheese" };
+        }
+        public override string ToString()
+        {
+            return "There are " + GroceryList.Length + "and they are:" + GroceryList;
+
+        }
+    }
+}
---
+++ EssentialTrainingTests/SimpleArrayTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SimpleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new Simple_Array();
+            Assert.AreEqual(testInstance.GroceryList.Length, 4);
+            Assert.AreEqual(testInstance.GroceryList[1], "Milk");
+
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new Simple_Array();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+          
+
+        }
+    }
+}
